f14f098c558611897f1441c1916c2bce195c75b0
Leonardo Maron <leo22.maron@hotmail.com>
2022-07-04 19:17:58 -0300

    Home, CardList, ViewImage

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,31 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { onOpen, onClose, isOpen } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
+  const [imageUrl, setImageUrl] = useState('');
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string) {
+    setImageUrl(url);
+    onOpen();
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid
+        columns={3}
+        spacing="10"
+      > 
+        { cards.map((card) => (
+          <Card 
+            key={card.id}
+            data={card}
+            viewImage={() => handleViewImage(card.url)}
+          />
+        )) }
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={imageUrl} />
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,19 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Card = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface ApiImagesResponse {
+  data: Card[];
+  after: string | null;
+}
+
 export default function Home(): JSX.Element {
   const {
     data,
@@ -18,18 +31,27 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    async ({ pageParam = null }) => {
+      const { data } = await api.get<ApiImagesResponse>('/api/images', { params: { after: pageParam } });
+
+      return data;  
+    },
+    {
+      getNextPageParam: (lastRequisitionInfos) => lastRequisitionInfos.after ?? null,
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.map((card) => card.data).flat()
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (!isLoading && isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +59,17 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        
+        { hasNextPage && (
+          <Button
+            isLoading={isFetchingNextPage}
+            loadingText="Carregando..."
+            onClick={() => fetchNextPage()}
+            mt='10'
+          >
+            Carregar mais
+          </Button>
+        ) }
       </Box>
     </>
   );
